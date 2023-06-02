f9dcf3b8d66ae8078174fc468339e672693df894
David Segura <david@segura.org>
2023-06-02 01:39:58 -0500

    Update Microsoft PowerShell 7.ps1

--- Winget Applications/Microsoft PowerShell 7.ps1
+++ Winget Applications/Microsoft PowerShell 7.ps1
@@ -12,6 +12,5 @@ else {
     Write-Error -Message 'Winget could not be installed.'
 }
 
-winget install --id Microsoft.PowerShell --exact --accept-source-agreements --accept-package-agreements
-
-winget install --id Microsoft.PowerShell --scope machine --override '/quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1' --accept-source-agreements --accept-package-agreements
+#winget install --id Microsoft.PowerShell --exact --accept-source-agreements --accept-package-agreements
+winget install --id Microsoft.PowerShell --exact --scope machine --override '/quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ADD_PATH=1' --accept-source-agreements --accept-package-agreements
