a40f3f632bd65ff50ebbe71fbccd848e250a4017
JM2K69 <bezettorres.jerome@neuf.fr>
2023-06-02 15:35:23 +0200

    23.6.2.1

---
+++ Winget Applications/Hashicorp Packer.ps1
@@ -0,0 +1,16 @@
+#Requires -RunAsAdministrator
+
+# Install Winget
+if (-not (Get-Command 'Winget' -ErrorAction SilentlyContinue)) {
+    Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe -Verbose
+}
+
+if (Get-Command 'Winget' -ErrorAction SilentlyContinue) {
+    winget
+}
+else {
+    Write-Error -Message 'Winget could not be installed.'
+}
+
+winget show --id Hashicorp.Packer --versions
+winget install --id=Hashicorp.Packer --exact
---
+++ Winget Applications/Hashicorp Vault.ps1
@@ -0,0 +1,17 @@
+#Requires -RunAsAdministrator
+
+# Install Winget
+if (-not (Get-Command 'Winget' -ErrorAction SilentlyContinue)) {
+    Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe -Verbose
+}
+
+if (Get-Command 'Winget' -ErrorAction SilentlyContinue) {
+    winget
+}
+else {
+    Write-Error -Message 'Winget could not be installed.'
+}
+
+winget show --id Hashicorp.Vault --versions
+winget install --id=Hashicorp.Vault --exact
+
