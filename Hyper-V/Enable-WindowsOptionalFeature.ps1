81103406e522047adf11d8cdd420c48a8ee22e2c
David Segura <david@segura.org>
2023-06-22 20:38:59 -0500

    23.6.22.1

--- Hyper-V/Enable-WindowsOptionalFeature.ps1
+++ Hyper-V/Enable-WindowsOptionalFeature.ps1
@@ -1,3 +1,14 @@
 #Requires -RunAsAdministrator
 
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart -Verbose
+$FeatureName = 'Microsoft-Hyper-V-All'
+$WindowsOptionalFeature = Get-WindowsOptionalFeature -Online -FeatureName $FeatureName -ErrorAction SilentlyContinue
+if ($WindowsOptionalFeature.State -eq 'Enabled') {
+    Write-Host -ForegroundColor Green "[+] Windows Optional Feature $FeatureName is installed"
+}
+elseif ($WindowsOptionalFeature.State -eq 'Disabled') {
+    Write-Host -ForegroundColor Yellow "[-] Enable-WindowsOptionalFeature -Online -FeatureName $FeatureName -NoRestart"
+    Enable-WindowsOptionalFeature -Online -FeatureName $FeatureName -NoRestart
+}
+else {
+    Write-Host -ForegroundColor Red "[!] $FeatureName is not compatible with this version of Windows"
+}
--- Windows Sandbox/Windows Sandbox Enable-WindowsOptionalFeature.ps1
+++ Windows Sandbox/Windows Sandbox Enable-WindowsOptionalFeature.ps1
@@ -4,7 +4,18 @@
 #dism /online /Enable-Feature /FeatureName:"Containers-DisposableClientVM" -All
 
 #Windows 11
-Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online -NoRestart -Verbose
+$FeatureName = 'Containers-DisposableClientVM'
+$WindowsOptionalFeature = Get-WindowsOptionalFeature -Online -FeatureName $FeatureName -ErrorAction SilentlyContinue
+if ($WindowsOptionalFeature.State -eq 'Enabled') {
+    Write-Host -ForegroundColor Green "[+] Windows Optional Feature $FeatureName is installed"
+}
+elseif ($WindowsOptionalFeature.State -eq 'Disabled') {
+    Write-Host -ForegroundColor Yellow "[-] Enable-WindowsOptionalFeature -Online -FeatureName $FeatureName -All -NoRestart"
+    Enable-WindowsOptionalFeature -Online -FeatureName $FeatureName -All -NoRestart
+}
+else {
+    Write-Host -ForegroundColor Red "[!] $FeatureName is not compatible with this version of Windows"
+}
 
 #If you're using a virtual machine, run the following PowerShell command to enable nested virtualization:
 #Set-VMProcessor -VMName <VMName> -ExposeVirtualizationExtensions $true
